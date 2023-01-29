6527a6ba6254e9a5073ef8d02d7137d094f923e2
Ankitasaha020 <ankitasaha250@gmail.com>
2023-01-29 16:14:26 +0530

    connected database success

--- server/config/connectdb.js
+++ server/config/connectdb.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async(DATABASE_URL) => {
     try {
         const DB_OPTIONS = {
-            dbName:"userAuth"
+            dbName:"userAuth",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         }
         await mongoose.connect(DATABASE_URL, DB_OPTIONS)
         console.log("Connected Sucessfully......")
