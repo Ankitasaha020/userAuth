f85d3f37c8cac0612c230631b86e0106c2d2dbdb
Ankitasaha020 <ankitasaha250@gmail.com>
2023-01-29 14:52:29 +0530

    db connected

--- server/server.js
+++ server/server.js
@@ -1,2 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
-// const express= require('express')
+import cors from "cors";
+import connectDB from './config/connectdb.js';
+
+const  app = express();
+const PORT = process.env.PORT || 5000;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// cors policy
+app.use(cors());
+
+// JSON
+app.use(express.json());
+
+// Connect Database
+connectDB(DATABASE_URL);
+
+// Routes
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+
